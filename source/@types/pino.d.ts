3b2dd21c00e3d16cac534c16bfd799b8bd4b69c4
Thiago Classen <tgclassen.dev@gmail.com>
2021-05-21 22:34:23 -0300

    few changes

---
+++ source/@types/pino.d.ts
@@ -0,0 +1,1 @@
+declare module '@elastic/ecs-pino-format';
--- source/config/Logger.ts
+++ source/config/Logger.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const logger = pino({
-    //...ecsFormat(),
+    ...ecsFormat(),
     level: process.env.LOG_LEVEL || 'info',
     prettyPrint: process.env.NODE_ENV !== 'production' || process.env.LOG_PRETTY_PRINT === 'true'
 });
